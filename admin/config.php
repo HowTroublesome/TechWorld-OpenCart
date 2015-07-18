340ea5e87ff53b494ff31768b1ad01b594c752d5
HowTroublesome <justforthestyle@abv.bg>
2015-07-18 14:58:18 +0300

    Added vQmod

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/TechWorld/admin/');
+define('HTTP_CATALOG', 'http://localhost/TechWorld/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/TechWorld/admin/');
+define('HTTPS_CATALOG', 'http://localhost/TechWorld/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Apps\WAMP\wamp\www\TechWorld/admin/');
+define('DIR_SYSTEM', 'F:\Apps\WAMP\wamp\www\TechWorld/system/');
+define('DIR_DATABASE', 'F:\Apps\WAMP\wamp\www\TechWorld/system/database/');
+define('DIR_LANGUAGE', 'F:\Apps\WAMP\wamp\www\TechWorld/admin/language/');
+define('DIR_TEMPLATE', 'F:\Apps\WAMP\wamp\www\TechWorld/admin/view/template/');
+define('DIR_CONFIG', 'F:\Apps\WAMP\wamp\www\TechWorld/system/config/');
+define('DIR_IMAGE', 'F:\Apps\WAMP\wamp\www\TechWorld/image/');
+define('DIR_CACHE', 'F:\Apps\WAMP\wamp\www\TechWorld/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Apps\WAMP\wamp\www\TechWorld/download/');
+define('DIR_LOGS', 'F:\Apps\WAMP\wamp\www\TechWorld/system/logs/');
+define('DIR_CATALOG', 'F:\Apps\WAMP\wamp\www\TechWorld/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'techworld');
+define('DB_PREFIX', 'oc_');
+?>
