db0dafeb5f0e0a7c7b3e8b698e919a0af37f6270
Bobby <borislav.galabov@gmail.com>
2015-07-18 14:37:10 +0300

    Installed OpenCart

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/TechWorld-OpenCart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/TechWorld-OpenCart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\TechWorld-OpenCart/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\TechWorld-OpenCart/system/');
+define('DIR_DATABASE', 'C:\wamp\www\TechWorld-OpenCart/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\TechWorld-OpenCart/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\TechWorld-OpenCart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\TechWorld-OpenCart/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\TechWorld-OpenCart/image/');
+define('DIR_CACHE', 'C:\wamp\www\TechWorld-OpenCart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\TechWorld-OpenCart/download/');
+define('DIR_LOGS', 'C:\wamp\www\TechWorld-OpenCart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'techworld');
+define('DB_PREFIX', 'oc_');
+?>
